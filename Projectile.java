52fde331d132cd678e54a59c7944719f6a0e03e9
claudioLitz <claudio_litz@estudante.sesisenai.org.com>
2025-11-26 09:33:41 -0300

    .

---
+++ Projectile.java
@@ -0,0 +1,50 @@
+
+import java.util.Arrays;
+
+public class Projectile {
+    public int posProjX;
+    public int posProjY;
+    public int[] origin;
+    public int accel;
+    public int dmg;
+    public String direction;
+
+
+    public Projectile(int posProjX, int posProjY, int[] origin, int accel, int dmg, String direction){
+        this.posProjX = posProjX;
+        this.posProjY = posProjY;
+        this.origin = origin;
+        this.accel = accel;
+        this.dmg = dmg;
+        this.direction = direction;
+
+
+    }
+
+    public void stats(){
+        System.out.println("Position x = " + this.posProjX);
+        System.out.println("Position y = " + this.posProjY);
+        System.out.println("Origin = " + Arrays.toString(this.origin));
+        System.out.println("Acceleration = " + this.accel);
+        System.out.println("damage = " + this.dmg);
+        System.out.println("direction = " + this.direction);
+    }
+
+    public void move(){
+
+    }
+
+    public void foreshadow(){
+
+    }
+
+    public void hitboxUPD(){
+
+
+    }
+
+
+
+
+
+}
