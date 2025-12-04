7e33ecdc7a476ad748319b9e6963580955af80c8
Aluno SENAI <Aluno@SN-389487.edu.fiesc.com.br>
2025-12-04 10:55:34 -0300

    ensinando o bruno

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,5 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
 
-        int[] a = {1,2};
-        int[] b = {1, 2, 3};
-
-
-        User user1 = new User(50,12,12,12,a,"right",1,b);
-        user1.ShowStatus();
-
-        //Projectile p1 = new Projectile(1, 1, null, 1, 10, null);
-        //p1.GetStatus();
     }
 }
