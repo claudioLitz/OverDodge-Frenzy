deb180f25189c03f02fcd40340998ded8c619378
claudioLitz <claudio_litz@estudante.sesisenai.org.com>
2025-12-04 08:58:21 -0300

    Test of create a hitbox object

--- hitBox.java
+++ hitBox.java
@@ -4,6 +4,26 @@ public class hitBox {
     protected int[] Coords;
     //public bool Phase;
 
+    protected void SetWidth(int width){
+        this.Width = width;
+    }
+
+    protected void SetHeight(int Height){
+        this.Height = Height;
+    }
+
+    protected void SetCoords (int[] Coords){
+        this.Coords = Coords;
+    }
+
+    protected void GetStatus(){
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+        System.out.println("Status:   ");
+        System.out.println("Width: " + this.Width);
+        System.out.println("Height: " + this.Height);
+
+    }
+    
     protected void verifyHit(){
 
     }
