6106da21e84fd93f7714a744005068f561830341
Aluno SENAI <Aluno@SN-389487.edu.fiesc.com.br>
2025-12-04 11:02:04 -0300

    calma claudiuo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import character.User;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
-
+        System.out.println("Hello world em decadencia!");
     }
 }
